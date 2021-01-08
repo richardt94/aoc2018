5cab05259c9e473c2ddb6afb2fc7dce0f1d8ab81
Richard Taylor <29562583+richardt94@users.noreply.github.com>
2021-01-08 20:04:29 +1100

    solve day 3

--- day3.py
+++ day3.py
@@ -4,10 +4,36 @@ claimrgx = re.compile("#([0-9]+) @ ([0-9]+),([0-9]+): ([0-9]+)x([0-9]+)")
 with open("input3.txt") as f:
     claims = f.readlines()
 
-claimed = set()
+claimed = dict()
 
+overlap = 0
 for claim in claims:
     m = claimrgx.match(claim)
     if not m:
         continue
+    x0, y0, sx, sy = (int(m.group(i)) for i in range(2,6))
+    for x in range(x0, x0+sx):
+        for y in range(y0, y0+sy):
+            if (x,y) in claimed:
+                if claimed[(x,y)] == 1:
+                    overlap += 1
+                claimed[(x,y)] += 1
+            else:
+                claimed[(x,y)] = 1
+
+print(overlap)
+
+#part 2
+for claim in claims:
+    m = claimrgx.match(claim)
+    if not m:
+        continue
+    cid, x0, y0, sx, sy = (int(m.group(i)) for i in range(1,6))
     
+    nooverlap = True
+    for x in range(x0, x0+sx):
+        for y in range(y0, y0+sy):
+            if claimed[(x,y)] != 1:
+                nooverlap = False
+    if nooverlap:
+        print(cid)
