0ce0450f5a851eba996ffcb223ea11089bbc9a1e
Richard Taylor <29562583+richardt94@users.noreply.github.com>
2021-01-14 22:18:35 +1100

    day 13 after correctly reading the problem spec

--- day13.py
+++ day13.py
@@ -1,58 +1,63 @@
 with open("input13.txt") as f:
     track = [list(l.rstrip("\n")) for l in f.readlines()]
 
-
-carts = dict()
+carts = []
 for y, l in enumerate(track):
     newl = ""
     for x, ch in enumerate(l):
         if ch == '>':
             newl += '-'
-            carts[(x,y)] = (1,0,0)
+            carts.append((x,y,1,0,0))
         elif ch == '<':
             newl += '-'
-            carts[(x,y)] = (-1,0,0)
+            carts.append((x,y,-1,0,0))
         elif ch == '^':
             newl += '|'
-            carts[(x,y)] = (0,-1,0)
+            carts.append((x,y,0,-1,0))
         elif ch == 'v':
             newl += '|'
-            carts[(x,y)] = (0,1,0)
+            carts.append((x,y,0,1,0))
 
-for _ in range(17710):
-    nxtcarts = dict()
-    crashed = []
-    for c in carts:
-        d = carts[c]
-        nc = (c[0] + d[0], c[1] + d[1])
-        if nc in nxtcarts:
-            crashed.append(nc)
-            continue
-        #need to detect "half-step" crashes -
-        #when this happens two carts will "swap"
-        #so it should be easy to detect
-        if c in nxtcarts and nc in carts:
-            crashed.append(c)
-            continue
-        c = nc
-        if track[c[1]][c[0]] == '/':
-            d = (-d[1], -d[0], d[2])
-        elif track[c[1]][c[0]] == '\\':
-            d = (d[1], d[0], d[2])
-        elif track[c[1]][c[0]] == '+':
-            if d[2] == 0:
-                if d[0]:
-                    d = (0, -d[0], d[2])
+while len(carts) > 1:
+    carts.sort(key = lambda x: x[0])
+    carts.sort(key = lambda x: x[1])
+    nxtcarts = []
+    crashed = [False for _ in carts]
+    for i, c in enumerate(carts):
+        nc = (c[0] + c[2], c[1] + c[3])
+        for j, c2 in enumerate(nxtcarts):
+            #ignore if something has already
+            #crashed into and destroyed this cart
+            if crashed[j]: continue
+            if c2[0] == nc[0] and c2[1] == nc[1]:
+                crashed[i] = True
+                crashed[j] = True
+        for j, c2 in enumerate(carts[i+1:]):
+            if crashed[j+i+1]: continue
+            if c2[0] == nc[0] and c2[1] == nc[1]:
+                crashed[i] = True
+                crashed[j+i+1] = True
+        d = c[2:5]
+        if track[nc[1]][nc[0]] == '/':
+            d = (-c[3], -c[2], c[4])
+        elif track[nc[1]][nc[0]] == '\\':
+            d = (c[3], c[2], c[4])
+        elif track[nc[1]][nc[0]] == '+':
+            if c[4] == 0:
+                if c[2]:
+                    d = (0, -c[2], c[4])
                 else:
-                    d = (d[1], 0, d[2])
-            elif d[2] == 2:
-                if d[0]:
-                    d = (0, d[0], d[2])
+                    d = (c[3], 0, c[4])
+            elif c[4] == 2:
+                if c[2]:
+                    d = (0, c[2], c[4])
                 else:
-                    d = (-d[1], 0, d[2])
+                    d = (-c[3], 0, c[4])
             d = (d[0], d[1], (d[2] + 1) % 3)
-        nxtcarts[c] = d
-    for c in crashed:
-        del nxtcarts[c]
-    carts = nxtcarts
+        nxtcarts.append((*nc,*d))
+    #clean up
+    carts = []
+    for i, c in enumerate(nxtcarts):
+        if not crashed[i]:
+            carts.append(c)
 print(carts)
