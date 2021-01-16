3aa2a86b646d512b491cfbe0f254e1abecddfc17
Richard Taylor <29562583+richardt94@users.noreply.github.com>
2021-01-16 21:02:32 +1100

    finally a correct solution for day 15

--- day15.py
+++ day15.py
@@ -1,33 +1,38 @@
 from collections import deque
-from time import sleep
 
 def display(dungeon, units):
     for y, l in enumerate(dungeon):
         scanl = ""
         for x, w in enumerate(l):
-            if (y,x) in units:
-                if units[(y,x)][0]:
-                    scanl += 'G'
-                else:
-                    scanl += 'E'
-            elif w:
-                scanl += '#'
+            for u in units:
+                if (y,x) == u[0:2] and not u[4]:
+                    if u[2]:
+                        scanl += 'G'
+                    else:
+                        scanl += 'E'
+                    break
             else:
-                scanl += '.'
+                if w:
+                    scanl += '#'
+                else:
+                    scanl += '.'
         print(scanl)
     print()
 
 direcs = [(0,1), (1,0), (-1,0), (0,-1)]
-def findmove(dungeon, units, spos):
+def findmove(dungeon, units, su):
     #squares in range
-    etype = 1 - units[spos][0]
+    etype = 1 - su[2]
     inr = set()
-    for u in units.items():
-        if u[1][0] == etype:
+    for u in units:
+        if u[2] == etype and not u[4]:
             for dr in direcs:
-                inr.add((u[0][0] + dr[0], u[0][1] + dr[1]))
+                inr.add((u[0] + dr[0], u[1] + dr[1]))
+    spos = (su[0], su[1])
+    if (su[0], su[1]) in inr:
+        return
 
-    occupied = {u for u in units}
+    occupied = {(u[0], u[1]) for u in units if not u[4]}
     #store only the first-reading-order
     #parent to each point in par
     par = {spos:(0,None)}
@@ -56,68 +61,76 @@ def findmove(dungeon, units, spos):
 
     return closest
 
-
 with open("input15.txt") as f:
     dmap = [l.strip() for l in f.readlines()]
-units = dict()
+unitsref = []
 dungeon = []
-nu = [0,0]
+nuref = [0,0]
 for y, l in enumerate(dmap):
     dl = []
     for x, ch in enumerate(l):
         if ch == 'E':
-            units[(y,x)] = (0,200)
-            nu[0] += 1
+            unitsref.append((y,x,0,200,False))
+            nuref[0] += 1
         elif ch == 'G':
-            units[(y,x)] = (1,200)
-            nu[1] += 1
+            unitsref.append((y,x,1,200,False))
+            nuref[1] += 1
         elif ch == '#':
             dl.append(True)
             continue
         dl.append(False)
     dungeon.append(dl)
 
-done = False
-nrounds = 0
-while True:
-    upos = list(units.keys())
-    upos.sort()
-    for up in upos:
-        #have i been killed?
-        if up not in units:
-            continue
-        etype = 1 - units[up][0]
-        if not nu[etype]:
-            done = True
+def playpower(power):
+    units = unitsref.copy()
+    nu = nuref.copy()
+    done = False
+    nrounds = 0
+    while True:
+        units.sort()
+        for (ui,unit) in enumerate(units):
+            #have i been killed?
+            if unit[4]:
+                continue
+            etype = 1 - unit[2]
+            if not nu[etype]:
+                done = True
+                break
+            #move
+            moveto = findmove(dungeon, units, unit)
+            if moveto:
+                units[ui] = (*moveto, *unit[2:])
+                unit = units[ui]
+
+            #attack if in range
+            adj = []
+            for dr in direcs:
+                epos = (unit[0] + dr[0], unit[1] + dr[1])
+                for i,u in enumerate(units):
+                    if u[0:2] == epos and u[2] == etype and not u[4]:
+                        adj.append((u[3],epos,i))
+
+            if adj:
+                ehp, epos, ei = min(adj)
+                ehp -= (3 if etype == 0 else power)
+                if ehp <= 0:
+                    if etype == 0:
+                        return 0
+                    units[ei] = (*epos, etype, ehp, True)
+                    nu[etype] -= 1
+                else:
+                    units[ei] = (*epos, etype, ehp, False)
+        if done:
             break
-        #move
-        moveto = findmove(dungeon, units, up)
-        if moveto:
-            ustats = units[up]
-            del units[up]
-            units[moveto] = ustats
-            up = moveto
+        nrounds += 1
 
-        #attack if in range
-        adj = []
-        for dr in direcs:
-            epos = (up[0] + dr[0], up[1] + dr[1])
-            if epos in units and units[epos][0] == etype:
-                adj.append((units[epos][1], epos))
-        if adj:
-            ehp, epos = min(adj)
-            if ehp <= 3:
-                del units[epos]
-                nu[etype] -= 1
-            else:
-                units[epos] = (etype, ehp - 3)
+    thp = sum(u[3] for u in units if not u[4])
+    
+    return thp*nrounds
 
-    display(dungeon, units)
-    if done:
-        break
-    nrounds += 1
-
-print(nrounds)
-thp = sum(u[1] for u in units.values())
-print(thp)
-print(thp*nrounds)
+score = 0
+power = 3
+while score == 0:
+    power += 1
+    score = playpower(power)
+print(power, score)
